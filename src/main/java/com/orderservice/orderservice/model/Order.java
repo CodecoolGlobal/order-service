bae47b75505e2b630ac48d528cb2d54a4d66b8f1
florindanciu <danciu2020@gmail.com>
2021-02-25 09:41:07 +0200

    Added @CreationTimestamp

--- src/main/java/com/orderservice/orderservice/model/Order.java
+++ src/main/java/com/orderservice/orderservice/model/Order.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -27,5 +28,6 @@ public class Order {
     private Long productId;
 
     @Column(name = "date")
+    @CreationTimestamp
     private LocalDateTime date;
 }
